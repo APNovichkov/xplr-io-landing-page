adb01f2a3a53afb55549ebf561f3249b4ed6178f
Andrey Novichkov <andrey.novichkov@gmail.com>
2019-10-30 22:42:01 -0700

    [add] Added heroku functionality with mongo

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ import re
 
 app = Flask(__name__)
 
-client = MongoClient()
-db = client['xplrdotio']
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/xplrdotio')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
 emails = db['emails']
 locations = db['locations']
 
@@ -64,4 +65,4 @@ def validate_email(input_email):
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
